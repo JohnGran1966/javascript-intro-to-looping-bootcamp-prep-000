d4c68ebdc95318e867634b94e2fd329a39395176
John Graniero <jgrani6167@aol.com>
2017-01-16 17:08:52 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,15 @@
+function forLoop(array){
+for (let i = 0; i < 25; i++){
+  if i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`
+}
+
+  forLoop(array)
+
+
+  let Time = 5; 
+while (Time >= 0) {
+
+      if (Time == 0) {
+       console.log("done");
+      }
+      console.log(--Time);
